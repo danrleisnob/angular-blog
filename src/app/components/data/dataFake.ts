19ee26004bc9fd58eab8628607dcb61c77b4faf5
danrleisnob <danrleimentalista@gmail.com>
2024-03-14 21:52:36 -0300

    finalizando routerPages

---
+++ src/app/components/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "Id":"1",
+    "photoCover": "https://angular.io/generated/images/marketing/home/any-size.svg",
+    "title": "saiu uma nova versão",
+    "description": "ola mundo",
+  },
+  {
+    "Id":"2",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmfQ7On4H4w3ASu0301qL03053TEsW7JLlJJfL-tiKdw&s",
+    "title": "saiu uma nova versão",
+    "description": "ola mundo",
+  },
+  {
+    "Id":"3",
+    "photoCover": "https://angular.io/generated/images/marketing/home/any-size.svg",
+    "title": "saiu uma nova versão",
+    "description": "ola mundo",
+  },
+  {
+    "Id":"4",
+    "photoCover": "https://angular.io/generated/images/marketing/home/any-size.svg",
+    "title": "saiu uma nova versão",
+    "description": "ola mundo",
+  },
+]
